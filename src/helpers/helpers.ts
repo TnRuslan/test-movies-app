0fd3113817906633275f9933f7d4ab5ab1a510c7
Ruslan Tendora <ruslantendora@gmail.com>
2023-03-12 23:09:36 +0200

    update readme

--- src/helpers/helpers.ts
+++
@@ -1,7 +0,0 @@
-// export const isFavorite = (id: string) => {
-//   const index = favoriteFilms.findIndex(f => f.imdbID === id);
-//   if (index === -1) {
-//     setIsFavoriteFilm(false);
-//   }
-//   setIsFavoriteFilm(true);
-// };
